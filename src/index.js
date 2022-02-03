086c804e9d9e0802153d57568312ffa7290a02e5
Adam Purcell <apurcell88@gmail.com>
2022-02-03 15:38:39 -0600

    Initial commit! Webpack is up and running. Bootstrap is incorporated. Created necessary HTML and JS files. Time to get to work tomorrow.

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import printMe from './inbox.js';
+
+function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+ 
+    element.innerHTML = 'Hello Webpack!';
+ 
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+ 
+    element.appendChild(btn);
+ 
+    return element;
+  }
+ 
+  document.body.appendChild(component());
